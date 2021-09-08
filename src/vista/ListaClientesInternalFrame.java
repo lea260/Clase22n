7e7d55fa9d058ede17866dbd7d69e01bc6bcf7a1
Leandro Calvetti <lcalvetti1@gmail.com>
2021-09-08 15:31:46 -0300

    crear Tabla clientes

--- src/vista/ListaClientesInternalFrame.java
+++ src/vista/ListaClientesInternalFrame.java
@@ -1,16 +1,30 @@
 package vista;
 
 import java.awt.EventQueue;
-
-import javax.swing.JInternalFrame;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
-import javax.swing.JPanel;
-import javax.swing.JButton;
 import java.awt.Font;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.JTableHeader;
+
+import entidades.ClienteEntidad;
+import modelo.Empresa;
+import utilidades.GestionCeldas02;
+import utilidades.GestionEncabezadoTabla;
+import utilidades.ModeloTabla;
+import utilidades.Utilidades;
 
 public class ListaClientesInternalFrame extends JInternalFrame {
-	private JTable table;
+	private JTable tablaCLientes;
+	private ArrayList<ClienteEntidad> listaClientes;
+	JScrollPane scrollPane;
+	public ModeloTabla modelo;
 
 	/**
 	 * Launch the application.
@@ -40,12 +54,12 @@ public class ListaClientesInternalFrame extends JInternalFrame {
 		setBounds(100, 100, 873, 800);
 		getContentPane().setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		scrollPane.setBounds(37, 63, 759, 400);
 		getContentPane().add(scrollPane);
 		
-		table = new JTable();
-		scrollPane.setViewportView(table);
+		tablaCLientes = new JTable();
+		scrollPane.setViewportView(tablaCLientes);
 		
 		JPanel panel = new JPanel();
 		panel.setBounds(37, 509, 759, 100);
@@ -53,9 +67,123 @@ public class ListaClientesInternalFrame extends JInternalFrame {
 		panel.setLayout(null);
 		
 		JButton btnAgregar = new JButton("Agregar");
-		btnAgregar.setFont(new Font("TeamViewer15", Font.PLAIN, 15));
+		btnAgregar.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		btnAgregar.setBounds(29, 31, 200, 40);
 		panel.add(btnAgregar);
+		construirTabla();
+	}
+	
+	private void construirTabla() {
+		// llamamos
+		Empresa empresa = new Empresa();
+		listaClientes   = empresa.mostrarDatosClientes();
+		// utiliza el metodo compareTo implementado en personaEntidad
+		//listaPersonas.sort(null);
+		//listaPersonas.sort(new OrdenarPersonaEdad());
+		//listaPersonas.sort(new OrdenarPersonaEdadId());
+		/*listaPersonas.sort((p1, p2) -> {
+			int valor = 0;
+			if (p1.getEdad() < p2.getEdad()) {
+				valor = -1;
+			} else if (p1.getEdad() > p1.getEdad()) {
+				valor = 1;
+			} else if (p1.getId() < p1.getId()) {
+				valor = -1;
+			} else if (p1.getId() > p1.getId()) {
+				valor = 1;
+			} else {
+				valor = 0;
+			}
+			return valor;
+		});*/
+		
+		//listaPersonas.sort((p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
+		
+		ArrayList<String> titulosList = new ArrayList<>();
 
+		titulosList.add("ID");
+		titulosList.add("Nombre");
+		titulosList.add("Edad");
+		
+		// se asignan las columnas al arreglo para enviarse al momento de construir la
+		// tabla
+		String titulos[] = new String[titulosList.size()];
+		for (int i = 0; i < titulos.length; i++) {
+			titulos[i] = titulosList.get(i);
+		}
+		/*
+		 * obtenemos los datos de la lista y los guardamos en la matriz que luego se
+		 * manda a construir la tabla
+		 */
+		Object[][] data = obtenerMatrizDatos(titulosList);
+		construirTabla(titulos, data);
+	}
+
+	private Object[][] obtenerMatrizDatos(ArrayList<String> titulosList) {
+
+		/*
+		 * se crea la matriz donde las filas son dinamicas pues corresponde a todos los
+		 * usuarios, mientras que las columnas son estaticas correspondiendo a las
+		 * columnas definidas por defecto
+		 */
+		String informacion[][] = new String[listaClientes.size()][titulosList.size()];
+
+		for (int x = 0; x < informacion.length; x++) {
+			informacion[x][Utilidades.ID] = listaClientes.get(x).getId() + "";
+			informacion[x][Utilidades.NOMBRE] = listaClientes.get(x).getNombre() + "";
+			informacion[x][Utilidades.EDAD] = listaClientes.get(x).getEdad() + "";			
+		}
+		return informacion;
+	}
+
+	private void construirTabla(String[] titulos, Object[][] data) {
+		modelo = new ModeloTabla(data, titulos);
+		// se asigna el modelo a la tabla
+		tablaCLientes.setModel(modelo);
+
+		// filasTabla=tablaPersonas.getRowCount();
+		// columnasTabla=tablaPersonas.getColumnCount();
+
+		// se asigna el tipo de dato que tendrán las celdas de cada columna definida
+		// respectivamente para validar su personalización
+
+		// tablaPersonas.getColumnModel().getColumn(Utilidades.ID).setCellRenderer(new
+		// GestionCeldas02("texto"));
+		// tablaPersonas.getColumnModel().getColumn(Utilidades.NOMBRE).setCellRenderer(new
+		// GestionCeldas02("texto"));
+		for (int i = 0; i < titulos.length ; i++) {// se resta 7 porque las ultimas 7 columnas se definen arriba
+			// System.out.println(i);
+			tablaCLientes.getColumnModel().getColumn(i).setCellRenderer(new GestionCeldas02("texto"));
+		}
+		tablaCLientes.getColumnModel().getColumn(Utilidades.SALDO).setCellRenderer(new GestionCeldas02("numerico"));
+
+		// se recorre y asigna el resto de celdas que serian las que almacenen datos de
+		// tipo texto
+
+		tablaCLientes.getTableHeader().setReorderingAllowed(false);
+		tablaCLientes.setRowHeight(25);// tamaño de las celdas
+		tablaCLientes.setGridColor(new java.awt.Color(0, 0, 0));
+		// Se define el tamaño de largo para cada columna y su contenido
+		tablaCLientes.getColumnModel().getColumn(Utilidades.ID).setPreferredWidth(100);// documento
+		tablaCLientes.getColumnModel().getColumn(Utilidades.NOMBRE).setPreferredWidth(200);// nombre
+		tablaCLientes.getColumnModel().getColumn(Utilidades.EDAD).setPreferredWidth(200);// direccion
+		tablaCLientes.getColumnModel().getColumn(Utilidades.SALDO).setPreferredWidth(100);// direccion
+
+		// personaliza el encabezado
+		JTableHeader jtableHeader = tablaCLientes.getTableHeader();
+		jtableHeader.setDefaultRenderer(new GestionEncabezadoTabla());
+		tablaCLientes.setRowSelectionAllowed(true);
+		
+		tablaCLientes.setTableHeader(jtableHeader);
+
+		// limitar seleccion a una fila
+		tablaCLientes.setRowSelectionAllowed(true);
+		tablaCLientes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		// se asigna la tabla al scrollPane
+		
+		
+
+		JScrollBar vertical = scrollPane.getVerticalScrollBar();
+		vertical.setValue(vertical.getMaximum());
 	}
 }
