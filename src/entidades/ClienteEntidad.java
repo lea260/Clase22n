96eb5fd55a79578a8759f4360d17a441e02135b1
Leandro Calvetti <lcalvetti@gmail.com>
2021-09-29 14:08:59 -0300

    ordenado por id, metodo compaare to

--- src/entidades/ClienteEntidad.java
+++ src/entidades/ClienteEntidad.java
@@ -1,38 +1,51 @@
 package entidades;
- 
+
 public class ClienteEntidad implements Comparable<ClienteEntidad> {
 	private int id;
 	private String nombre;
 	private int edad;
+
 	public ClienteEntidad(int id, String nombre, int edad) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.edad = edad;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+
 	@Override
-	public int compareTo(ClienteEntidad o) {
+	public int compareTo(ClienteEntidad cli) {
 		// TODO Auto-generated method stub
-		return 0;
+		int retornar = 0;
+		if (this.id < cli.id) {
+			retornar = -1;
+		} else if (this.id > cli.id) {
+			retornar = -1;
+		}
+		return retornar;
 	}
-		
-	
+
 }
